bfd7f88fc1b46aa1d195e40e0a57e1f0745b9b91
bmwoolf <bradleymwoolf@gmail.com>
2020-11-04 22:41:23 -0800

    initial setup

---
+++ calculator.py
@@ -0,0 +1,10 @@
+    """
+    Calculator library with basic arithmetic for the purposes of testing CI
+    """
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
